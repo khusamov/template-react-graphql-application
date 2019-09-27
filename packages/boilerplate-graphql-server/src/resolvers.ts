b4db8332382b05b5ebcb78cb75cee85ae1edcbcd
Khusamov Sukhrob <khusamov@yandex.ru>
2019-09-27 17:58:19 +0300

    Добавлен пример выборки полей из запроса selectionSet.selections

--- packages/boilerplate-graphql-server/src/resolvers.ts
+++ packages/boilerplate-graphql-server/src/resolvers.ts
@@ -1,6 +1,30 @@
+import {FieldNode, FragmentSpreadNode, GraphQLResolveInfo} from 'graphql';
+
 export default {
-	user({id}, context, info) {
-		console.log(info)
+	user({id}, context, info: GraphQLResolveInfo) {
+
+
+		let selectionNames: string[] = [];
+		const userFieldNode = info.fieldNodes[0];
+		if (userFieldNode.selectionSet) {
+
+			selectionNames = (
+				userFieldNode.selectionSet.selections.filter(selection => 'name' in selection) as (FieldNode | FragmentSpreadNode)[]
+			).map(selection => selection.name.value)
+		}
+
+		// Список полей, которые были выбраны в запросе
+		// Например selectionNames [ 'id', 'name', 'email' ] из следующего запроса:
+		// {
+		//   user(id: 1) {
+		//     id
+		//     name
+		//     email
+		//   }
+		// }
+		//
+		console.log('selectionNames', selectionNames);
+
 		return {
 			id: 1,
 			name: 'Петр'
